4cf752e54736217742b9c658fce883117788a7af
JakePescetti <34384649+JakePescetti@users.noreply.github.com>
2018-04-15 23:53:41 -0700

    Create campusflaskscript.py

---
+++ CampusTest/campusflaskscript.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+import math
+import pickle
+#from datetime import *
+
+app = Flask(__name__)
+
+
+@app.route('/data', methods=['POST', 'GET'])
+def names():
+	
+	if request.method == 'GET':
+		timeToGrnFile = open("gps.pkl","r")#debug was time.pkl
+		time = pickle.load(timeToGrnFile)
+		return jsonify(time)
+
+	if request.method == 'POST':
+		content = request.get_json()
+		if content is not None:
+			fileObject = open("gps.pkl","wb") 
+			pickle.dump(content,fileObject)   
+			fileObject.close()
+			return jsonify(content)
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0')
