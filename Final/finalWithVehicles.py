082fc35a8a5195e3982d3420c681bbe211bf6492
JakePescetti <34384649+JakePescetti@users.noreply.github.com>
2018-05-06 22:40:11 -0700

    Add files via upload

--- Final/finalWithVehicles.py
+++ Final/finalWithVehicles.py
@@ -239,6 +239,7 @@ def run():
 	traci.vehicle.add("bike", "bike1", typeID='typeBike')  #Adds bike to simulation
 	traci.vehicle.setSpeed("bike",0)
 	oldTLSID = ''
+	warnCounter=0
 	while True:
 		starttime=time.time()
 		updatePosition()
@@ -253,11 +254,12 @@ def run():
 			#this must be processed in the simulation, can1t be done in the webserver it will break things
 			#likely because it is not thread safe
 			
-			if tank != oldTLSID:
+			if tank != oldTLSID and step-warnCounter > 5:
 				clearCars(oldTLSID)
-			elif upcominglights[0][2] < 5 and BikeSpeed != 0: #Check if cyclist id crossing intersection for warning beacon
+			elif upcominglights[0][2] < 5 and BikeSpeed != 0: #Check if cyclist is crossing intersection for warning beacon
 				oldTLSID = tank
 				warnCars(tank)
+				warnCounter = step
 			
 		traci.simulationStep()
 		step += 1
