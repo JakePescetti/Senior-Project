8353ee7fced02124eff6f63297b1b646e90316f1
JakePescetti <34384649+JakePescetti@users.noreply.github.com>
2018-05-10 10:56:14 -0700

    Add files via upload

--- Final/finalWithVehiclesRadius.py
+++ Final/finalWithVehiclesRadius.py
@@ -309,9 +309,10 @@ def warnCars(tlsID):
 def clearCars():
 	allCars = traci.vehicle.getIDList()
 	for car in allCars:
-		traci.vehicle.setColor(car,(255,255,0,255)) #default yellow color 255,255,0,255
-		traci.vehicle.slowDown(car,traci.vehicle.getSpeedWithoutTraCI(car),3000)
-		print('car cleared\n')
+		if car != "bike":
+			traci.vehicle.setColor(car,(255,255,0,255)) #default yellow color 255,255,0,255
+			traci.vehicle.slowDown(car,traci.vehicle.getSpeedWithoutTraCI(car),3000)
+			print('car cleared\n')
 		# if traci.vehicle.isStopped(car):
 			# traci.vehicle.resume(car) #clear cars to go again
 			#print('car cleared\n')
