facd70a2e20cc56748f9195e0e18faf5e4ea3421
JakePescetti <34384649+JakePescetti@users.noreply.github.com>
2018-05-18 10:40:33 -0700

    Add files via upload

--- BricRoutes/bric.py
+++ BricRoutes/bric.py
@@ -176,7 +176,7 @@ def EngineOn():
 	# this is the normal way of using traci. sumo is started as a
 	# subprocess and then the python script connects and runs
 	traci.start([sumoBinary, "-c", "bric.sumocfg",
-							 "--full-output", "tripinfo.sbx"]) #may need to add  "--ignore-route-errors"
+							 "--full-output", "tripinfo.xml", "--emission-output", "emissions.xml"]) #may need to add  "--ignore-route-errors"
 	run()
 	
 
